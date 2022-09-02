f0709ac97654bb0978f296e0f8df5aeaa27dd48a
Aman Shukla <amanks7400@gmail.com>
2022-09-02 09:31:38 +0530

    Add Search Modal

--- components/layout/Appbar.js
+++ components/layout/Appbar.js
@@ -1,7 +1,13 @@
-import { Box, HStack, IconButton, Text } from "@chakra-ui/react";
+import { Box, Button, HStack, IconButton, Input, InputGroup, InputLeftElement, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Text, useDisclosure } from "@chakra-ui/react";
 import { Icon } from "@iconify/react";
 
 export default function Appbar() {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+
+    function handleSearchModal() {
+        onOpen();
+    }
+
     return (
         <HStack justifyContent={"space-between"} p={4}>
             {/* Filter */}
@@ -14,9 +20,40 @@ export default function Appbar() {
             </Text>
             {/* Search & Notification */}
             <HStack >
-                <IconButton isRound icon={<Icon icon="fa6-solid:magnifying-glass-location" />} />
+                <IconButton onClick={handleSearchModal} isRound icon={<Icon icon="fa6-solid:magnifying-glass-location" />} />
                 <IconButton variant="ghost" icon={<Icon icon="clarity:notification-solid-badged" />} />
             </HStack>
+            <SearchModal {...{isOpen, onClose}} />
         </HStack>
     )
 }
+
+function SearchModal({isOpen, onClose}) {
+    return (
+      <>
+    
+  
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>
+                <InputGroup>
+                <InputLeftElement>
+                <Icon icon="carbon:airport-location" />
+                </InputLeftElement>
+                <Input variant={"filled"} placeholder="Search keywords,name.." />
+                </InputGroup>
+            </ModalHeader>
+            <ModalBody>
+            </ModalBody>
+  
+            <ModalFooter>
+              <Button size="sm" colorScheme='purple'  onClick={onClose}>
+                Close
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </>
+    )
+  }
